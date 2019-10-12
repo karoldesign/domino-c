85aef6d084c18ad67559980af27b1cda4bbee711
karol <carolina.chamorro@securitasdirect.es>
2019-10-12 19:56:14 +0200

    first commit

--- practica-1-domino/main.cpp
+++ practica-1-domino/main.cpp
@@ -6,10 +6,52 @@
 //  Copyright © 2019 Carolina Chamorro. All rights reserved.
 //
 
+#include <time.h>
+#include <cstdlib>
+#include <list>
 #include <iostream>
+
+using namespace std;
+
+// Ficha
+string fileToStr(short int left, short int right) {
+    // content
+    return 0;
+}
+
+string toStr(int n) {
+    return 0;
+}
+
+bool putLeft(string table, short int fileN1, short int fileN2) {
+    return 0;
+}
+bool putRight(string table, short int fichaN1, short int fichaN2) {
+    return 0;
+}
+
+// Jugador
+
+// Tablero
+void mostrarTablero(short int fileN1, short int fileN2, string table, int numCounter, int numStolen) {
+    return;
+}
+// Menú
+int showMenu() {
+    // content
+    return 0;
+}
+
+// Alert
+short int aleat() {}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << " -------------------- " << endl;
+    cout << "|       TABLERO      |" << endl;
+    cout << " -------------------- " << endl;
+    cout << " -------------------- " << endl;
+    cout << "|  MENÚ DE OPCIONES  |" << endl;
+    cout << " -------------------- " << endl;
     return 0;
 }
