e6feba218ef3ee355cba1c4356b19e70aa02ec30
karol <carolina.chamorro@securitasdirect.es>
2019-10-12 21:43:19 +0200

    feat: add logic

--- practica-1-domino/main.cpp
+++ practica-1-domino/main.cpp
@@ -14,6 +14,29 @@
 using namespace std;
 
 string toStr(int n) {
+    switch (n) {
+        case 0:
+            return "0";
+        case 1:
+            return "1";
+        case 2:
+            return "2";
+        case 3:
+            return "3";
+        case 4:
+            return "4";
+        case 5:
+            return "5";
+        case 6:
+            return "6";
+        case 7:
+            return "7";
+        case 8:
+            return "8";
+        case 9:
+            return "9";
+    }
+    
     return 0;
 }
 
@@ -24,19 +47,19 @@ string fichaToStr(short int left, short int right) {
 }
 
 bool canPutLeft(string table, short int tokenN1, short int tokenN2) {
-    return 0;
+    return (table[1]-0x30==tokenN2);
 }
 
 bool canPutRight(string table, short int tokenN1, short int tokenN2) {
-    return 0;
+    return (table[table.size()-2]-0x30==tokenN1);
 }
 
 string putTokenLeft(string table, short int tokenN1, short int tokenN2) {
-    return 0;
+    return table + fichaToStr(tokenN1, tokenN2);
 }
 
 string putTokenRight(string table, short int tokenN1, short int tokenN2) {
-    return 0;
+    return table + fichaToStr(tokenN1, tokenN2);
 }
 
 // Tablero
@@ -77,6 +100,9 @@ short int aleat() {
 }
 
 int main(int argc, const char * argv[]) {
+    // insert code here...
+    int counter = 0, stolen = 0, option = 0;
     srand(time(NULL));
+    
     return 0;
 }
