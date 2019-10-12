44f2bf63b669ea60aa8bc4b788faf2ad7c8c730d
karol <carolina.chamorro@securitasdirect.es>
2019-10-12 19:26:11 +0200

    Initial Commit

---
+++ practica-1-domino/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practica-1-domino
+//
+//  Created by Carolina Chamorro on 12/10/2019.
+//  Copyright © 2019 Carolina Chamorro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
